13e9544d63c98bd918c9a4ee0125c2c580cd28a8
romanhadjisergis <rhadjisergis@hotmail.com>
2022-12-09 16:06:48 +0000

    did some more stuff

--- book-flix/src/hooks/useFetch.js
+++ book-flix/src/hooks/useFetch.js
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react";
-import env from "react-dotenv";
+// import env from "react-dotenv";
 
 function useFetch() {
+  const URL = process.env.REACT_APP_BACKEND_URL ?? "http://localhost:3000";
+
   const [books, setBook] = useState([]);
 
   async function fetchBook() {
-    const response = await fetch(`${env.URL}api/books/search`);
+    const response = await fetch(`${URL}api/books/search`);
     const data = await response.json();
     console.log("book data: ", data);
     setBook(data.payload);
